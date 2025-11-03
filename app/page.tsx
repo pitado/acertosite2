d5dc2b6ff9216431c28e59d8c73fb98bff7dd188
Miguel Pita <Miguellpitaa@gmail.com>
2025-11-03 14:41:14 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{display:"grid",placeItems:"center",minHeight:"100vh"}}>
+      <a href="/login">Ir para Login</a>
+    </main>
+  );
+}
