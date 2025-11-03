a43b2775f7038d0c9594372d0cdd3f69108ada72
Miguel Pita <Miguellpitaa@gmail.com>
2025-11-03 14:40:59 -0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{display:"grid",placeItems:"center",minHeight:"100vh"}}>
-      <a href="/login">Ir para Login</a>
-    </main>
-  );
-}
