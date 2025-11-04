93f0101b6a58a11f11018cddd9e4c74c1df0d621
Miguel Pita <Miguellpitaa@gmail.com>
2025-11-04 09:13:06 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,2 @@
 import { redirect } from "next/navigation";
-
-export default function Page() {
-  // Quando o usuário abrir a raiz "/", vai direto para a tela de login
-  redirect("/login");
-}
+export default function Page() { redirect("/login"); }
