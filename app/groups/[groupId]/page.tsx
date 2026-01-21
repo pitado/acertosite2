2438bdb96f30b280164e1df5891329f5afa6e797
Miguel Pita <Miguellpitaa@gmail.com>
2026-01-21 16:16:26 -0300

    Delete app/groups/[groupId] directory

--- app/groups/[groupId]/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/groups/[groupId]/page.tsx
-import GroupPageClient from "./GroupPageClient";
-
-export default function GroupPage({ params }: { params: { groupId: string } }) {
-  return <GroupPageClient groupId={params.groupId} />;
-}
